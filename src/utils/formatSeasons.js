5a537a7930c56486bd14f41e2600d8fe6835152d
manyshyn <manyshynyuri@gmail.com>
2020-05-06 21:31:06 -0600

    Remove log, update packages

--- src/utils/formatSeasons.js
+++ src/utils/formatSeasons.js
@@ -1,11 +1,10 @@
-export const formatSeasons = allEpisodes => {
+export const formatSeasons = (allEpisodes) => {
   const seasons = {};
-  allEpisodes.forEach(e => {
+  allEpisodes.forEach((e) => {
     if (!seasons.hasOwnProperty(`Season ${e.season}`)) {
       seasons[`Season ${e.season}`] = [];
     }
     seasons[`Season ${e.season}`].push(e);
   });
-  console.log(seasons);
   return seasons;
 };
